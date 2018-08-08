e766e4d0f4e4f8a1a10ef44c00ddf0d813cf6930
fendy <281966728@qq.com>
2018-08-09 07:35:15 +0800

    router遍历controller文件夹自动注册 展示启动时间

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const Koa = require('koa');
 const app = new Koa();
 const config = require('./config/config.default');
 const beforeStartUp = require('./frame/beforeStartUp');
+const start = Date.now();
 
 beforeStartUp(app).then(() => {
     app.listen(config.port, () => {
         console.log(`app is starting at port ${config.port}`);
+        console.log(`启动用时${(Date.now() - start) / 1000}s`);
     });
 });
--- frame/beforeStartUp.js
+++ frame/beforeStartUp.js
@@ -14,6 +14,7 @@ function pathname (name) {
     return path.join(__dirname, `../app${name}`);
 }
 
+// 对fs.readdir的Promise化的封装
 function readDir(dirPath) {
     return new Promise((res, rej) => {
         fs.readdir(dirPath, (err, file) => {
@@ -25,9 +26,53 @@ function readDir(dirPath) {
     });
 }
 
+// 遍历指定目录下的文件或文件夹，执行回调
+async function mapDir (url, callback) {
+    const dirList = await readDir(url);
+
+    for (let i = 0;i < dirList.length;i++) {
+        let filename = dirList[i];
+
+        if (filename.includes('.js')) {
+            const content = require(`${url}/${filename}`);
+
+            filename = filename.replace('.js', '');
+            callback(`${url}/${filename}`, filename, content);
+        } else {
+            await mapDir(`${url}/${filename}`, callback);
+        }
+    }
+}
+
 // 自动挂载api
 async function mountApiRouter() {
+    const router = new Router();
+    const api = new Router();
+    const page = new Router();
 
+    // 挂载页面controller
+    await mapDir(PageCtrlPath, (pathname, filename, content) => {
+        pathname = pathname.replace(PageCtrlPath, '');
+        for (const [key, func] of Object.entries(content)) {
+            page.get(`${pathname}/${key}`, func);
+        }
+    });
+    // 挂载接口api
+    await mapDir(ApiCtrlPath, (pathname, filename, content) => {
+        pathname = pathname.replace(ApiCtrlPath, '');
+        if (content.methods) {
+            for (const [key, method] of Object.entries(content.methods)) {
+                api[method](`${pathname}/${key}`, content[key]);
+            }
+        } else {
+            console.log(`[error]:文件${filename}缺少methods对象`)
+        }
+    });
+
+    router.use('/', page.routes(), page.allowedMethods());
+    router.use('/api', api.routes(), api.allowedMethods());
+
+    return router;
 }
 
 const PageCtrlPath = pathname('/controller/page');
@@ -47,33 +92,7 @@ module.exports = async app => {
     /**
      * 注册路由
      * */
-    const router = new Router();
-    const api = new Router();
-    const page = new Router();
-    const pageDir = await readDir(PageCtrlPath);
-    const apiDir = await readDir(ApiCtrlPath);
-
-    apiDir.forEach(dir => {
-        if (dir.includes('.js')) {
-            const apiList = require(`${ApiCtrlPath}/${dir}`);
-
-            dir = dir.replace('.js', '');
-            if (apiList.methods) {
-                for (const [key, method] of Object.entries(apiList.methods)) {
-                    api[method](`/${dir}/${key}`, apiList[key]);
-                }
-            } else {
-                console.log(`[error]:文件${dir}缺少methods对象`)
-            }
-        } else {
-            readDir(`${ApiCtrlPath}/${dir}`).then(file => {
-
-            });
-        }
-    });
-    console.log(1111);
-    router.use('/', page.routes(), page.allowedMethods());
-    router.use('/api', api.routes(), api.allowedMethods());
+    const router = await mountApiRouter();
     app.use(router.routes());
     app.use(router.allowedMethods());
 };
